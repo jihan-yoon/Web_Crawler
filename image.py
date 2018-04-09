d65a08d764a7fcd4e31320671923dd2edf44aa6a
jihan <aofk159@naver.com>
2018-04-09 17:22:47 +0900

    image.py modified

--- image.py
+++ image.py
@@ -5,10 +5,13 @@ f = open('urls.txt','w')
 
 html = urlopen('https://search.shopping.naver.com/search/all.nhn?query=%EB%82%A8%EC%9E%90+%EB%A7%A8%ED%88%AC%EB%A7%A8&cat_id=&frm=NVSHATC')
 soup = BeautifulSoup(html,"lxml")
-eee = soup.find_all("img")
+eee = soup.find_all('img')
+goods_list = soup.find_all('div',{'class' : 'img_area'})
+
+print(eee)
 
 for m in eee:
-	f.write(m.get('src'))
-	f.write("\n")
+	f.write(str(m.get('data-original')))
+	f.write('\n')
 
 f.close
