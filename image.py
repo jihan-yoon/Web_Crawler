b71b73bb47c06d22feaec5b5872a35a7d4ef3e55
jihan <aofk159@naver.com>
2018-04-10 14:41:24 +0900

    DaatBase 로드 및 입력 코드 완료

--- image.py
+++ image.py
@@ -1,7 +1,30 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+# import sqlite3
+import pymysql.cursors
+import MySQLdb
 
 f = open('images_src.txt','w')
+
+import pymysql.cursors
+
+conn = pymysql.connect(host='localhost',
+		user='root',
+		password='sorj1256',
+		db='jh',
+		charset='utf8mb4')	
+
+
+with conn.cursor() as cursor:
+	sql = '''
+	CREATE TABLE mtm_data (
+	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	goods_url varchar(255) NOT NULL,
+	image_src varchar(255) NOT NULL
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8
+	'''
+	cursor.execute(sql)
+conn.commit()
 
 for num in range(1,100) :
 	s = str(num)
@@ -13,11 +36,20 @@ for num in range(1,100) :
 	eee = soup.find_all('img')
 	uuu = soup.find_all('a')
 	
-	print(uuu)
-
-	for m in eee:
-		f.write(str(m.get('data-original')))
-		f.write('\n')
+	for u in uuu:
+		for m in eee:
+			i = 1;
+			f.write(str(m.get('data-original')))
+			f.write('\n')
+			
+			with conn.cursor() as cursor:
+				sql = 'INSERT INTO mtm_data (goods_url, image_src) VALUES (%s, %s)'
+				cursor.execute(sql, (str(u.get('href')), str(m.get('data-original'))))
+				
+			conn.commit()
+			
+			i = i + 1;
+		
 	num = num + 1;
 
 f.close
