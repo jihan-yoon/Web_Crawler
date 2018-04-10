1dddae09e87b8daf3f2e434cc70306e0069e0c57
jihan <aofk159@naver.com>
2018-04-10 11:23:43 +0900

    image_src.txt finish

--- image.py
+++ image.py
@@ -1,17 +1,23 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-f = open('urls.txt','w')
+f = open('images_src.txt','w')
 
-html = urlopen('https://search.shopping.naver.com/search/all.nhn?query=%EB%82%A8%EC%9E%90+%EB%A7%A8%ED%88%AC%EB%A7%A8&cat_id=&frm=NVSHATC')
-soup = BeautifulSoup(html,"lxml")
-eee = soup.find_all('img')
-goods_list = soup.find_all('div',{'class' : 'img_area'})
+for num in range(1,100) :
+	s = str(num)
+	str1 = 'https://search.shopping.naver.com/search/all.nhn?origQuery=%EB%A7%A8%ED%88%AC%EB%A7%A8&pagingIndex='
+	str2 = '&pagingSize=80&viewType=list&sort=rel&frm=NVSHPAG&query=%EB%A7%A8%ED%88%AC%EB%A7%A8' # pagingSize : 한 페이지 제품 개수
+	useurl = str1 + s + str2
+	html = urlopen(useurl)
+	soup = BeautifulSoup(html,"lxml")
+	eee = soup.find_all('img')
+	uuu = soup.find_all('a')
+	
+	print(uuu)
 
-print(eee)
-
-for m in eee:
-	f.write(str(m.get('data-original')))
-	f.write('\n')
+	for m in eee:
+		f.write(str(m.get('data-original')))
+		f.write('\n')
+	num = num + 1;
 
 f.close
